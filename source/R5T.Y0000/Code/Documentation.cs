0e09558ae2ece56e4948201895cfc1d9a9578f9c
David Coats <david@getsafetycone.com>
2022-02-14 10:44:22 -0800

    Add initial namespace name documentation.

--- source/R5T.Y0000/Code/Documentation.cs
+++ source/R5T.Y0000/Code/Documentation.cs
@@ -6,7 +6,7 @@ namespace R5T.Y0000
     /// <summary>
     /// Documentation for .NET, C# language, and standard library concepts.
     /// </summary>
-    public static class Documentation
+    public static partial class Documentation
     {
     }
 }
---
+++ source/R5T.Y0000/Code/Documentation/ForNamespaceName.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace R5T.Y0000
+{
+    public static partial class Documentation
+    {
+        public static class ForNamespaceName
+        {
+            /// <summary>
+            /// <inheritdoc cref="InvalidNamespaceName" path="/definition"/>
+            /// </summary>
+            /// <definition>Invalid namespace names contain invalid characters, are unescaped (non-contextual) keywords, or have the verbatim specifier (@) after the leading character.</definition>
+            public static readonly object InvalidNamespaceName;
+
+            /// <summary>
+            /// <inheritdoc cref="RecommendedNamespaceName" path="/definition"/>
+            /// </summary>
+            /// <definition>Recommended namespace names start with a capital letter, and then have either uppercase and lowercase letters, digits, or underscores.</definition>
+            public static readonly object RecommendedNamespaceName;
+
+            /// <summary>
+            /// <inheritdoc cref="UnrecommendedNamespaceName" path="/definition"/>
+            /// </summary>
+            /// <definition>Unrecommended namespace names are valid, but start with something other than a capital letter.</definition>
+            public static readonly object UnrecommendedNamespaceName;
+
+            /// <summary>
+            /// <inheritdoc cref="ValidNamespaceName" path="/definition"/>
+            /// </summary>
+            /// <definition>Valid namespace names contain only valid characters, are escaped keywords, or have the verbatim specifier as the leading character.</definition>
+            public static readonly object ValidNamespaceName;
+        }
+    }
+}
