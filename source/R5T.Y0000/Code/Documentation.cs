dc25a33a0ef14be62570e6cacf2d2fe532f280e7
davidcoats <david@getsafetycone.com>
2023-09-04 14:06:10 -0600

    Interim changes.

--- source/R5T.Y0000/Code/Documentation.cs
+++ source/R5T.Y0000/Code/Documentation.cs
@@ -8,5 +8,20 @@ namespace R5T.Y0000
     /// </summary>
     public static partial class Documentation
     {
+        /// <summary>
+        /// Search pattern strings are used by <see cref="System.IO.Directory.EnumerateDirectories(string, string)"/>,
+        /// for example.
+        /// <para>
+        /// Because search pattern functionality is so limited, it's better to just enumerate all files,
+        /// then use a regex on the file path to be more specific.
+        /// </para>
+        /// </summary>
+        /// <remarks>
+        /// The search pattern is pretty minimal, it offers only two wildcard characters:
+        /// <para>* - Matches zero-or-more characters.</para>
+        /// <para>? - Matches exactly one character.</para>
+        /// See "Remarks" here: <see href="https://learn.microsoft.com/en-us/dotnet/api/system.io.directory.getfiles?view=net-7.0"/>
+        /// </remarks>
+        public static readonly object For_SearchPattern;
     }
 }
